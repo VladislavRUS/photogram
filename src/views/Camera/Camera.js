fd3152ccdf9cd3ac6b9da55bf1e407287e8a6024
Vladislav Kurochkin <vlad555.95@mail.ru>
2018-10-26 01:51:04 +0400

    Лоадер при загрузке изображения

--- src/views/Camera/Camera.js
+++ src/views/Camera/Camera.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import { Image, Wrapper } from './Camera.styles';
+import { Image, Wrapper, LoaderWrapper } from './Camera.styles';
 import { observer } from 'mobx-react';
+import { observable } from 'mobx';
+import { ActivityIndicator } from 'react-native';
 
 @observer
 class Camera extends React.Component {
+  @observable
+  isLoading = false;
+
   render() {
     const { navigation } = this.props;
     const imageUri = navigation.getParam('uri');
 
     return (
       <Wrapper>
-        <Image source={{ uri: imageUri }} resizeMode={'contain'} />
+        <Image
+          source={{ uri: imageUri }}
+          resizeMode={'contain'}
+          onLoadEnd={() => (this.isLoading = false)}
+          onLoadStart={() => (this.isLoading = true)}
+        />
+        {this.isLoading && (
+          <LoaderWrapper>
+            <ActivityIndicator color={'#f02861'} />
+          </LoaderWrapper>
+        )}
       </Wrapper>
     );
   }
--- src/views/Camera/Camera.styles.js
+++ src/views/Camera/Camera.styles.js
@@ -12,3 +12,15 @@ export const Image = styled.Image`
   width: 100%;
   height: 100%;
 `;
+
+export const LoaderWrapper = styled.View`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  top: 0;
+  z-index: 10;
+`;
